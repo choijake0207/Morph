298da6071a8aeceec3fe24459033048052d6a64a
choijake0207 <jake.ch0712@gmail.com>
2025-02-22 08:53:10 -0800

    add skeumorph cardsyyle

---
+++ src/Components/Skeumorphic.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/skeumorphic.css"
+
+export default function Skeumorphic() {
+  return (
+    <div className="skeumorphic card">
+      
+    </div>
+  )
+}
