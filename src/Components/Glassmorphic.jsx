fe24fbd2b0986830330c933754cf8b68b9e12588
choijake0207 <jake.ch0712@gmail.com>
2025-02-22 09:40:44 -0800

    add glassmorphic card / fix lightmode stlyes

---
+++ src/Components/Glassmorphic.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../styles/glassmorphic.css"
+
+export default function Glassmorphic() {
+  return (
+
+        
+        <div className="glassmorphic_wrap card">
+            <div className="glassmorphic"></div>
+            <div className="glass_blobs">
+                <div className="one"></div>
+                <div className="two"></div>
+                <div className="three"></div>
+            </div>
+        
+        </div>
+
+  )
+}
