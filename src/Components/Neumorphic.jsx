bf3216b65d3e68e98958c20f146b7c5b6f809766
choijake0207 <jake.ch0712@gmail.com>
2025-02-22 08:44:36 -0800

    add neumorph card style

---
+++ src/Components/Neumorphic.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/neumorphic.css"
+
+export default function Neumorphic() {
+  return (
+    <div className="neumorphic card">
+      
+    </div>
+  )
+}
